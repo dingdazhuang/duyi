3711aa5c4fed693333189545c87d64ab954668b4
dingdazhuang <1021797587@qq.com>
2019-02-25 19:14:30 +0800

    try

---
+++ index.js
@@ -0,0 +1,9 @@
+var oUl = $('ul');
+var olis = $('li');
+var len = olis.length;
+var ulWidth = parseInt(oUl.css('width'));
+var ot = Math.floor((ulWidth - 400) / (len - 1));
+function init() {
+  
+}
+init();
